fbfd1a6528b2e0f8ec43e025556e6c4693273874
Vadim <iskolen@mail.ru>
2024-02-26 15:43:17 +0300

    Создал ветку для 1-ой лабораторной работы

---
+++ Valuator/TextEvaluator.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+
+namespace Valuator
+{
+    public class TextEvaluator
+    {
+        public static double CalculateRank(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return 0.0;
+
+            int totalCharacters = text.Length;
+            int nonAlphabeticCharacters = text.Count(c => !char.IsLetter(c));
+
+            return (double)nonAlphabeticCharacters / totalCharacters;
+        }
+
+        public static int CalculateSimilarity(string text, IDatabase db, string currentKey)
+        {
+            if (IsDuplicate(text, db, currentKey))
+                return 1;
+            else
+                return 0;
+        }
+
+        private static bool IsDuplicate(string text, IDatabase db, string currentKey)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+
+            const string textKeyPrefix = "TEXT-";
+
+            var textKeys = db.Multiplexer.GetServer("127.0.0.1:6379").Keys(pattern: textKeyPrefix + "*");
+
+            foreach (var key in textKeys)
+            {
+                if (key != currentKey)
+                {
+                    string storedText = db.StringGet(key);
+
+                    if (string.Equals(storedText, text, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
